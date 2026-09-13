8e4ecc9fa86d066c05a4b382fd61bbbe23c967cc
PavelBaranov <p204368@gmail.com>
2026-09-13 15:57:34 +0300

    feat: hide admin button from header, change admin URL to /backend-admin/

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ sitemaps = {
 }
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("backend-admin/", admin.site.urls),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("", include("core.urls")),
